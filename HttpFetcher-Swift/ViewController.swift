06858e9935c14bf43798faaa9fbbbd976fe4f6e0
HirokiUmatani <o9093009555@gmail.com>
2015-11-03 10:42:58 +0900

    add project file

---
+++ HttpFetcher-Swift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HttpFetcher-Swift
+//
+//  Created by hirokiumatani on 2015/11/03.
+//  Copyright © 2015年 hirokiumatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ HttpFetcher-SwiftTests/HttpFetcher_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  HttpFetcher_SwiftTests.swift
+//  HttpFetcher-SwiftTests
+//
+//  Created by hirokiumatani on 2015/11/03.
+//  Copyright © 2015年 hirokiumatani. All rights reserved.
+//
+
+import XCTest
+@testable import HttpFetcher_Swift
+
+class HttpFetcher_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
