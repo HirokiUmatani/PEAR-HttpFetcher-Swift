7191ec1ab3e9c8aa397849592bc8efa25dd0d1fa
HirokiUmatani <o9093009555@gmail.com>
2015-11-26 01:03:15 +0900

    re upload file

--- HttpFetcher-Swift/ViewController.swift
+++ HttpFetcher-Swift/ViewController.swift
@@ -14,19 +14,66 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        let fetcher:HttpFetcher = HttpFetcher()
-        fetcher.asyncFetch(
+        /* HTTP SYNC GET*/
+        let syncfetcherG:HttpFetcher = HttpFetcher()
+        syncfetcherG.syncFetchGET(
+            urlString: "http://blog.pear.chat",
+            success: { (data:NSData)
+                -> Void in
+                NSLog("HTTP SYNC GET \n%@", NSString(data:data, encoding:NSUTF8StringEncoding)!)
+            })
+            {
+                (error:NSError)
+                -> Void in
+        }
+        
+        
+        /* HTTP ASYNC GET*/
+        let asyncfetcherG:HttpFetcher = HttpFetcher()
+        asyncfetcherG.asyncFetchGET(
             urlString: "http://pear.chat",
             success:
             {
                 (data:NSData)
                 -> Void in
-            
+                NSLog("HTTP ASYNC GET \n%@",  NSString(data:data, encoding:NSUTF8StringEncoding)!)
             })
             {
                 (error:NSError)
                 -> Void in
-            }
+        }
+        
+        /* HTTP SYNC POST*/
+        let syncfetcherP:HttpFetcher = HttpFetcher()
+        syncfetcherP.syncFetchPOST(
+            urlString: "http://blog.pear.chat",
+            parameter:nil,
+            success:
+            {
+                (data:NSData)
+                -> Void in
+                NSLog("HTTP SYNC POST \n%@",  NSString(data:data, encoding:NSUTF8StringEncoding)!)
+            })
+            {
+                (error:NSError)
+                -> Void in
+        }
+        
+        /* HTTP ASYNC POST*/
+        let asyncfetcherP:HttpFetcher = HttpFetcher()
+        asyncfetcherP.asyncFetchPOST(
+            urlString: "http://pear.chat",
+            parameter:nil,
+            success:
+            {
+                (data:NSData)
+                -> Void in
+                NSLog("HTTP ASYNC POST \n%@",  NSString(data:data, encoding:NSUTF8StringEncoding)!)
+            })
+            {
+                (error:NSError)
+                -> Void in
+        }
     }
 
     override func didReceiveMemoryWarning()
--- PEAR-HttpFetcher-Swift/HttpFetcher.swift
+++ PEAR-HttpFetcher-Swift/HttpFetcher.swift
@@ -7,38 +7,162 @@
 //
 import UIKit
 
-public class HttpFetcher: NSObject
+public class HttpFetcher: NSObject,NSURLSessionDelegate
 {
     /**
-     async http reqest
+     async http reqest GET
      */
-    public func asyncFetch(
+    public func asyncFetchGET(
         urlString urlString:String ,
         success:(NSData) -> Void ,
         failed:(NSError) -> Void)
     {
-        
-        let url:NSURL = NSURL(string:urlString)!
-
-        let request:NSURLRequest  = NSURLRequest(
-            URL: url)
-
-        NSURLConnection.sendAsynchronousRequest(
-            request,
-            queue: NSOperationQueue.mainQueue())
+        let config:NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session:NSURLSession = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        let url:NSURL = NSURL(string: urlString)!
+        let urlRequest:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "GET"
+        urlRequest.timeoutInterval = 15
+        let task = session.dataTaskWithRequest(urlRequest)
+        {
+            (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+            dispatch_async(dispatch_get_main_queue(),
             {
-                (res:NSURLResponse?,
-                data:NSData?,
-                error:NSError?) -> Void in
-                
-                if (error != nil)
+                if(error == nil)
                 {
-                    failed(error!)
+                    let httpResp:NSHTTPURLResponse = response as! NSHTTPURLResponse
+                    if(httpResp.statusCode == 200)
+                    {
+                        success(data!)
+                    }
                 }
                 else
                 {
-                    success(data!)
+                    failed(error!)
                 }
-            }
+            })
+            
+        }
+        task.resume()
     }
+    
+    /**
+     sync http reqest GET
+     */
+    public func syncFetchGET(
+        urlString urlString:String ,
+        success:(NSData) -> Void ,
+        failed:(NSError) -> Void)
+    {
+        let config:NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session:NSURLSession = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        let url:NSURL = NSURL(string: urlString)!
+        let urlRequest:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "GET"
+        urlRequest.timeoutInterval = 15
+        let task = session.dataTaskWithRequest(urlRequest)
+            {
+                (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+                
+                if(error == nil)
+                {
+                    let httpResp:NSHTTPURLResponse = response as! NSHTTPURLResponse
+                    if(httpResp.statusCode == 200)
+                    {
+                        success(data!)
+                    }
+                }
+                else
+                {
+                    failed(error!)
+                }
+        
+                
+        }
+        task.resume()
+    }
+    
+    /**
+     async http reqest POST
+     */
+    public func asyncFetchPOST(
+        urlString urlString:String ,
+        parameter param:NSData!,
+        success:(NSData) -> Void ,
+        failed:(NSError) -> Void)
+    {
+        let config:NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session:NSURLSession = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        let url:NSURL = NSURL(string: urlString)!
+        let urlRequest:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "POST"
+        urlRequest.timeoutInterval = 15
+        urlRequest.HTTPBody = param
+        
+        let task = session.dataTaskWithRequest(urlRequest)
+            {
+                (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+                dispatch_async(dispatch_get_main_queue(),
+                    {
+                        if(error == nil)
+                        {
+                            let httpResp:NSHTTPURLResponse = response as! NSHTTPURLResponse
+                            if(httpResp.statusCode == 200)
+                            {
+                                success(data!)
+                            }
+                        }
+                        else
+                        {
+                            failed(error!)
+                        }
+                })
+                
+                
+        }
+        task.resume()
+    }
+    
+    /**
+     sync http reqest POST
+     */
+    public func syncFetchPOST(
+        urlString urlString:String ,
+        parameter param:NSData!,
+        success:(NSData) -> Void ,
+        failed:(NSError) -> Void)
+    {
+        let config:NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session:NSURLSession = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        let url:NSURL = NSURL(string: urlString)!
+        let urlRequest:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "POST"
+        urlRequest.timeoutInterval = 15
+        urlRequest.HTTPBody = param
+        
+        let task = session.dataTaskWithRequest(urlRequest)
+            {
+                (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+                
+                if(error == nil)
+                {
+                    let httpResp:NSHTTPURLResponse = response as! NSHTTPURLResponse
+                    if(httpResp.statusCode == 200)
+                    {
+                        success(data!)
+                    }
+                }
+                else
+                {
+                    failed(error!)
+                }
+                
+                
+                
+        }
+        task.resume()
+    }
+
+
+
 }
